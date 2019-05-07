5b52c630bf3674905d5283258e829683948cbd51
Ming@MingPC1 <ming.jiang.biw1@gmal.com>
2019-05-06 22:52:26 -0400

    add Serverside.aspx test consume web API using c# request

---
+++ WebApplication2/ServerSide.aspx
@@ -0,0 +1,35 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="ServerSide.aspx.cs" Inherits="WebApplication2.ServerSide" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="Content/bootstrap.min.css" rel="stylesheet">
+    <link href="Content/site.css" rel="stylesheet">
+    <script src="Scripts/jquery-1.9.1.min.js"></script>
+    <script src="Scripts/bootstrap.min.js"></script>
+    <script src="Scripts/webapi.js"></script>
+</head>
+<body>
+<div class="container">
+    <header class="jumbotron"><%-- image set in site.css --%></header>
+    <main>    
+        <form id="form1" runat="server">
+            <div class="row">
+                <div class="col-sm-12 table-responsive">
+                    <h1>Categories</h1>
+                    <asp:GridView ID="grdCategories" runat="server"
+                        CssClass="table table-bordered table-condensed table-striped"
+                        OnPreRender="grdCategories_PreRender">
+                        <HeaderStyle CssClass="bg-halloween" />
+                    </asp:GridView>
+                </div>
+            </div>
+        </form>
+
+    </main>
+</div>
+</body>
+</html>
---
+++ WebApplication2/ServerSide.aspx.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Net;
+using System.IO;
+using System.Data;
+
+namespace WebApplication2
+{
+    public partial class ServerSide : System.Web.UI.Page
+    {
+        //configure web request object
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string url = "http://localhost:50106/api/categories/";
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "GET";
+            request.ContentType = "text/xml; encoding='utf-8'";
+
+            //send request, get xml response and convert to stream
+            WebResponse response = request.GetResponse();
+            Stream stream = response.GetResponseStream();
+
+            //read stream into a dataset
+            DataSet ds = new DataSet();
+            ds.ReadXml(stream);
+
+            //bind data set to gridview
+            grdCategories.DataSource = ds.Tables[0];
+            grdCategories.DataBind();
+
+        }
+        protected void grdCategories_PreRender(object sender, EventArgs e)
+        {
+            grdCategories.HeaderRow.TableSection = TableRowSection.TableHeader;
+        }
+    }
+}
