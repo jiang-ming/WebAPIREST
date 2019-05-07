91bcdc2e939dda2e041c328bd54b74779d419ca5
Ming@MingPC1 <ming.jiang.biw1@gmal.com>
2019-05-06 22:28:38 -0400

    init

---
+++ WebApplication2/Controllers/CategoriesController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApplication2.Controllers
+{
+
+
+    public class CategoriesController : ApiController
+    {
+        CategoryDB data;
+        public CategoriesController()
+        {
+            this.data = new CategoryDB();
+        }
+
+        // GET: api/categories
+        public IEnumerable<Category> GetCategories()
+        {
+            //System.Threading.Thread.Sleep(3000); // 3 seconds
+            return data.GetCategories();
+        }
+
+        // GET: api/categories/masks
+        public Category GetCategoryById(string id)
+        {
+            return data.GetCategoryById(id);
+        }
+        // GET: api/categories/?name=Masks
+        public IEnumerable<Category> GetCategoriesByShortName(string name)
+        {
+            return data.GetCategoriesByShortName(name);
+        }
+        // POST(Insert): api/categories
+        public int PostCategory([FromBody]Category value)
+        {
+            return data.InsertCategory(value);
+        }
+        // PUT(Update): api/categories/masks
+        public int PutCategory(string id, [FromBody] Category value)
+        {
+            value.CategoryID = id;
+            return data.UpdateCategory(value);
+        }
+        // DELETE: api/categories/masks
+        public int DeleteCategory(string id)
+        {
+            Category value = new Category() { CategoryID = id };
+            return data.DeleteCategory(value);
+        }
+
+    }
+}
---
+++ WebApplication2/Scripts/webapi.js
@@ -0,0 +1,84 @@
+﻿var api = "http://localhost:50106/api/categories/";
+
+$(document).ready(function () {
+    displayCategories();
+    $(document.body).on('click', 'a', function (e) {
+        e.preventDefault();
+        findCategory($(this).attr("href"));
+    });
+});
+function displayCategories() {
+    $.getJSON(api, function (data) {
+        var rows = "";
+        $.each(data, function (key, val) {
+            rows += "<tr><td><a href=" + api + val.CategoryID + ">"
+                + val.CategoryID + "</a></td>";
+            rows += "<td>" + val.ShortName + "</td>";
+            rows += "<td>" + val.LongName + "</td></tr>";
+        });
+        $('#categories > tbody tr').remove();
+        $('#categories > tbody').append(rows);
+        clearAll();
+    })
+        .fail(showError);
+};
+function findCategory(href) {
+    $('#message').html("Finding...");
+    $.getJSON(href, function (data) {
+        $('#id').val(data.CategoryID);
+        $("#id").attr("disabled", "disabled");
+        $('#short').val(data.ShortName);
+        $('#long').val(data.LongName);
+        $('#message').text("");
+    })
+        .fail(showError);
+};
+function insertCat() {
+    $('#message').text("Inserting...");
+    $.ajax({
+        type: 'POST',
+        url: api,
+        data: $('#form1').serialize(),
+        dataType: "json",
+        success: displayCategories,
+        error: showError
+    });
+};
+function updateCat() {
+    $('#message').text("Updating...");
+    $.ajax({
+        type: 'PUT',
+        url: api + $('#id').val(),
+        data: $('#form1').serialize(),
+        dataType: "json",
+        success: displayCategories,
+        error: showError
+    });
+};
+function deleteCat() {
+    $('#message').text("Deleting...");
+    $.ajax({
+        type: 'DELETE',
+        url: api + $('#id').val(),
+        data: $('#form1').serialize(),
+        dataType: "json",
+        success: displayCategories,
+        error: showError
+    });
+};
+
+function showError(request, status, error) {
+    try {
+        var response = JSON.parse(request.responseText);
+        $('#message').text(response.ExceptionMessage);
+    } catch (e) {
+        $('#message').text("An error occurred - unable to contact the server.");
+    }
+};
+function clearAll() {
+    $('#id').val("");
+    $("#id").removeAttr("disabled");
+    $('#short').val("");
+    $('#long').val("");
+    $('#message').text("");
+};
